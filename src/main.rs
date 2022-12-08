8c44ea4ae9a971fd5fbfbbf00d95b629b7bc81a4
cezary17 <cezarykujath@gmail.com>
2022-12-08 15:46:43 +0100

    day 7

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 mod line_reader;
 
+
 mod d1;
 mod d2;
 mod d3;
 mod d4;
 mod d5;
 mod d6;
+mod d7;
+
 
 fn main() {
-    println!("Day 6 Task 1");
-    d6::task1();
-    println!("Day 6 Task 2");
-    d6::task2();
+    println!("Day 7 Task 1");
+    d7::task1("src/input_sources/day7_input.txt");
+    println!("Day 7 Task 2");
+    d7::task2("src/input_sources/day7_input.txt");
 }
